4728524ccc82749a0814297f1e7ead345df20f9e
TamarKhutchua <126088392+TamarKhutchua@users.noreply.github.com>
2023-09-01 21:13:50 +0400

    extend objects

--- app.js
+++ app.js
@@ -1,13 +1,39 @@
-let car = {
-  make: "Jeep",
-  model: "Wrangler",
-  accelerate: function () {
-    return this.model + " drives away";
-  },
-  brake: function () {
-    return this.make + " pulls up";
-  },
+// let car = {
+//   make: "Jeep",
+//   model: "Wrangler",
+//   accelerate: function () {
+//     return this.model + " drives away";
+//   },
+//   brake: function () {
+//     return this.make + " pulls up";
+//   },
+// };
+// console.log("Car is a " + car.make + "" + car["model"]);
+// console.log(car.accelerate());
+// console.log(car.brake());
+
+let property,
+  car = {
+    make: "Jeep",
+    model: "Wrangler",
+    accelerate: function () {
+      return this.model + " drives away";
+    },
+    brake: function () {
+      return this.make + " pulls up";
+    },
+  };
+for (property in car) {
+  console.log(property + ": " + car[property]);
+}
+car.make = "Ford";
+car.model = "Bronco";
+car.engine = "V6";
+car.start = function () {
+  return this.make + " motor is running";
 };
-console.log("Car is a " + car.make + "" + car["model"]);
+console.log("\nCar is a " + car.make + "" + car["model"]);
+console.log("Engine Type: " + car.engine);
+console.log(car.start());
 console.log(car.accelerate());
 console.log(car.brake());
