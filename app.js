616ee6a1e3cf6d88523d853549ac4f8dd25401ab
TamarKhutchua <126088392+TamarKhutchua@users.noreply.github.com>
2023-09-01 21:50:49 +0400

    loop elements

--- app.js
+++ app.js
@@ -63,3 +63,27 @@ for (month in summer) {
 }
 console.log("Start of Summer: " + summer[0]);
 console.log(summer);
+
+let i,
+  result,
+  boolArray = [];
+console.log("Fill Elements...");
+for (i = 1; i < 11; i++) {
+  boolArray[i] = i % 2 === 0 ? true : false;
+  console.log("Element " + i + ": " + boolArray[i]);
+}
+console.log("Read Elements...");
+result = "";
+for (i = 1; i < boolArray.length; i++) {
+  if (boolArray[i]) {
+    result += i + " | ";
+  }
+}
+console.log("True in Elements: " + result);
+result = "";
+for (i = 1; i < boolArray.length; i++) {
+  if (!boolArray[i]) {
+    result += i + " | ";
+  }
+}
+console.log("False in Elements: " + result);
