b280800f808b7bbe6a0ecd5e7d1f05ce64c7a7e0
TamarKhutchua <126088392+TamarKhutchua@users.noreply.github.com>
2023-09-02 13:40:03 +0400

    meet JSON

--- app2.js
+++ app2.js
@@ -14,3 +14,18 @@ const format = /.+\@.+\..+/;
 console.log(email + " Valid? " + format.test(email));
 email += ".com";
 console.log(email + " Valid? " + format.test(email));
+
+let obj = {
+  category: "Fashion",
+  models: [
+    { name: "Alice", age: 21, city: "New York" },
+    { name: "Kelly", age: 23, city: "Las Vegas" },
+  ],
+};
+let json_obj = JSON.stringify(obj);
+console.log(json_obj);
+let new_obj = JSON.parse(json_obj);
+console.log(new_obj);
+console.log(new_obj["category"]);
+console.log(new_obj.models[0].name);
+console.log(new_obj["models"][1]["name"]);
