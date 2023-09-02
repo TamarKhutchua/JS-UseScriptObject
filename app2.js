83c922e9d82f8ee81c24255e3e121259a1a8813d
TamarKhutchua <126088392+TamarKhutchua@users.noreply.github.com>
2023-09-02 13:50:49 +0400

    make promises

--- app2.js
+++ app2.js
@@ -29,3 +29,24 @@ console.log(new_obj);
 console.log(new_obj["category"]);
 console.log(new_obj.models[0].name);
 console.log(new_obj["models"][1]["name"]);
+
+const promise = new Promise(function (resolve, reject) {
+  let random = Math.round(Math.random() * 10);
+  if (random % 2 === 0) {
+    setTimeout(function () {
+      resolve(random);
+    }, 1000);
+  } else {
+    setTimeout(function () {
+      reject(random);
+    }, 1000);
+  }
+});
+promise
+  .then(console.log(promise))
+  .then(function (res) {
+    console.log(res + " Is Even");
+  })
+  .catch(function (err) {
+    console.log(err + " Is Odd");
+  });
