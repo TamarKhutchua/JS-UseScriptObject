c81b32e99e0ae4250474a30742e7c41380d01617
TamarKhutchua <126088392+TamarKhutchua@users.noreply.github.com>
2023-09-02 13:28:34 +0400

    Match Patterns

---
+++ app2.js
@@ -0,0 +1,16 @@
+const system = "Windows",
+  suite = "Office",
+  pattern = /ice/i;
+console.log("In " + system + "? " + pattern.test(system));
+console.log("In " + suite + "?" + pattern.test(suite));
+let result = pattern.exec(suite);
+if (result) {
+  console.log("Found: " + result + " at " + result.index);
+} else {
+  console.log("No Match Found");
+}
+let email = "mike@example";
+const format = /.+\@.+\..+/;
+console.log(email + " Valid? " + format.test(email));
+email += ".com";
+console.log(email + " Valid? " + format.test(email));
