f5028528eea69cdd5ddffeadc1c5fff303703f93
TamarKhutchua <126088392+TamarKhutchua@users.noreply.github.com>
2023-09-02 14:03:50 +0400

    fetch data

--- app2.js
+++ app2.js
@@ -50,3 +50,16 @@ promise
   .catch(function (err) {
     console.log(err + " Is Odd");
   });
+
+// fetch("http://localhost/weekdays.json")
+//   .then((response) => response.json())
+//   .then((data) => list(data))
+//   .catch((err) => console.log(err));
+// function list(data) {
+//   const values = Object.values(data);
+//   let i = 0;
+//   while (i < values.length) {
+//     console.log(values[i]);
+//     i++;
+//   }
+// }
