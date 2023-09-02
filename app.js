7ec1f26cbbcb91a3aaab6c22ccdf77ed7f8cc68a
TamarKhutchua <126088392+TamarKhutchua@users.noreply.github.com>
2023-09-02 13:08:14 +0400

    Set Dates

--- app.js
+++ app.js
@@ -202,3 +202,18 @@ if (hour > 12) {
   hour -= 12;
 }
 console.log("Time is: " + hour + ":" + minute + suffix);
+
+const holiday = new Date(2025, 6, 4);
+console.log("Object: " + holiday);
+holiday.setFullYear(2028);
+holiday.setMonth(11);
+holiday.setDate(25);
+holiday.setHours(12);
+holiday.setMinutes(0);
+holiday.setSeconds(0);
+holiday.setMilliseconds(0);
+console.log("String: " + holiday.toString());
+console.log("UTC: " + holiday.toUTCString());
+console.log("Locale: " + holiday.toLocaleString());
+console.log("Date: " + holiday.toDateString());
+console.log("Time: " + holiday.toTimeString());
